4e36da4ea0443e0664d711dc3a8a1bca16410cac
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-02-15 10:39:06 +0600

    first problem completed

---
+++ problems/1/solution.js
@@ -0,0 +1,11 @@
+var initialMoney = 1000;
+var orangePricePerKG = 200;
+var applePricePerKG = 300;
+
+var productsBought = 2 * orangePricePerKG + applePricePerKG;
+
+var remainingMoney = initialMoney - productsBought;
+
+console.log(initialMoney);
+console.log(productsBought);
+console.log(remainingMoney);
