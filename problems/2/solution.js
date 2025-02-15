c6211da8f7c1cce881427f3bf2136b9ac6bdec30
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-02-15 10:53:38 +0600

    second problem completed

---
+++ problems/2/solution.js
@@ -0,0 +1,17 @@
+var mathematicsMarks = 75.25;
+var biologyMarks = 65;
+var chemistryMarks = 80;
+var physicsMarks = 35.45;
+var banglaMarks = 99.5;
+
+var totalSubjectCount = 5;
+
+var avarageMarks =
+  (mathematicsMarks +
+    biologyMarks +
+    chemistryMarks +
+    physicsMarks +
+    banglaMarks) /
+  totalSubjectCount;
+
+console.log(avarageMarks.toFixed(2));
